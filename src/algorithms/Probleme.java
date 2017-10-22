9ca79fca07cc5646928568f9821d5566bafa851b
tlasou <thibault.lasou@u-psud.fr>
2017-10-22 15:42:11 +0200

    Définition de Probleme et Algorithme

---
+++ src/algorithms/Probleme.java
@@ -0,0 +1,17 @@
+package algorithms;
+
+import java.util.ArrayList;
+
+abstract public class Probleme
+{
+	protected ArrayList<Integer> varPremierNiv;
+	
+	final public int fonctionObj()
+	{
+		return fonctionObj(varPremierNiv);
+	}
+	
+	abstract public int fonctionObj(ArrayList<Integer> vars); // Evalue la fonction objectif avec les valeurs des variables passées en paramètres
+	abstract public boolean constraints(ArrayList<Integer> vars); // Determine si les valeurs des variables passées en paramètres respectent les contraintes
+	abstract public ArrayList<Integer> voisinage(); // retourne des valeurs des variables principales selon le voisinage choisi pour le problème
+}
